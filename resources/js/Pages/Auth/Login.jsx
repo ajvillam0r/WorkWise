374a1e3124f47111213c50cdaa9453bffcf6a669
AJSyntax <ajvillamor45@gmail.com>
2025-10-12 20:24:44 +0800

    app.blade.php update

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -40,7 +40,7 @@ export default function Login({ status, canResetPassword }) {
 
     return (
         <>
-            <Head title="Log in - WorkWise" />
+            <Head title="Log in" />
             <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap" rel="stylesheet" />
 
             <div className="relative min-h-screen bg-white">
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title inertia>{{ config('app.name', 'WorkWise') }}</title>
+        <title inertia></title>
 
         <!-- Favicon -->
         <link rel="icon" type="image/png" href="{{ asset('image/WorkWise_logo.png') }}">
